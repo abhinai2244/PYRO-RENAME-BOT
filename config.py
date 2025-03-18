3548f0e5f8c7342fb51344665c511aa28dd7103f
Abhinai Reddy⛔️ <159715958+abhinai2244@users.noreply.github.com>
2025-03-18 17:51:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://ibb.co/gM8LxSzs")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5756495153').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "-1002497873969") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1002616517280"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
     MAX_CONCURRENT_TRANSMISSIONS = int(os.environ.get("MAX_CONCURRENT_TRANSMISSIONS", "2")) # Set the maximum amount of concurrent transmissions (uploads & downloads).
     
     # wes response configuration     
